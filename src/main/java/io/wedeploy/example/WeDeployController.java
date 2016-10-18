9688a182f7c70fe7f8023ad75f008f9edee8b2e0
Ivan Santos <pragmaticivan@gmail.com>
2016-10-17 17:40:33 -0700

    Clean Controller

--- src/main/java/io/wedeploy/example/WeDeployController.java
+++ src/main/java/io/wedeploy/example/WeDeployController.java
@@ -1,35 +1,26 @@
 package io.wedeploy.example;
 
-import javax.validation.Valid;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @EnableAutoConfiguration
 public class WeDeployController {
 
-	public WeDeployController() {
+    public WeDeployController() {
 
-	}
+    }
 
-	@RequestMapping("/")
-	public ModelAndView hello() {
-		return new ModelAndView("layout");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WeDeployController.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(WeDeployController.class, args);
-	}
+    @RequestMapping("/")
+    public ModelAndView hello() {
+        return new ModelAndView("layout");
+    }
 
 }
