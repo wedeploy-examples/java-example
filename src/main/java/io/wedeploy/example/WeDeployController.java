1b63d2fd64493a8df66987d2fed1fd74c5fd50f8
Ivan Santos <pragmaticivan@gmail.com>
2016-10-17 17:27:13 -0700

    Remove @author

--- src/main/java/io/wedeploy/example/WeDeployController.java
+++ src/main/java/io/wedeploy/example/WeDeployController.java
@@ -15,9 +15,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * @author Ivan Santos
- */
 @Controller
 @EnableAutoConfiguration
 public class WeDeployController {
